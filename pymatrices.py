d875571cfab7b193bea44b6be3970517c86ee8a5
DumbleDouche <www.ronan.c@gmail.com>
2018-02-10 00:48:29 +0100

    Add of basic Matrix Class

--- pymatrices.py
+++ pymatrices.py
@@ -4,5 +4,11 @@ Very modest try at making a library on Matrix operations in Python
 """
 
 __version__ = "1.0"
-__all__     = None
+__all__     = ['Matrix']
 
+class Matrix:
+
+    def __init__(self, x, y, default=0):
+        self.nrows = x
+        self.ncols  = y
+        self.array = [[default for point in range(self.ncols)] for row in range(self.nrows)]
