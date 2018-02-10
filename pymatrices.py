121a798c0a8c67d36e9cc3be06ba7c63fad5e0ad
DumbleDouche <www.ronan.c@gmail.com>
2018-02-11 00:01:15 +0100

    Changed the class structure, Matrix can be instanciated by specifying size or a valid array as input. Added `arrayShapeAssert()` to ensure a given array is a valid matrix and return size. Added `inputTypeAssert()` to ensure input types are correct. Added `newMatrix()` to create a new matrix by giving a value for each point, and the size of the matrix

--- pymatrices.py
+++ pymatrices.py
@@ -1,33 +1,65 @@
 """
 Very modest attempt at making a library on Matrix operations in Python
-
+This isn't probably the fastest thing I could come up with, i just 
+tried to make it relatively easy to debug.
+I'll probably have a version, without any debugging elements, which i'll
+use whenever their use in the general algorithm is safe and sound.
 """
 
-__version__ = "1.0"
+__version__ = "1.1"
 __all__     = ['Matrix']
 
-def   matrix(array):
+def   inputTypeAssert(inputs):
+    """
+    Asserting Input Type
+    """
+    for el in inputs:
+        if not isinstance(el[0], el[1]):
+            raise ValueError("Parameter |" + str(el[0]) + 
+            "| is of type %s but should be of type %s"
+            % (el[0].__class__.__name__, el[1].__name__))
+
+def   arrayShapeAssert(array):
     try:
-        nrows = len(array)
+        rows = len(array)
     except TypeError:
-        raise TypeError("Object of type", type(array), "should a two-dimension array")
+        raise TypeError("Object of", type(array), "should a two-dimension array")
     try:
-        ncolumns = len(array[0])
+        columns = len(array[0])
     except IndexError:
         raise IndexError("Object should be a two-dimensionnal array")
     for i, row in enumerate(array):
-        if len(row) != ncolumns:
-            raise ValueError("Dimensions are not consistent: [%d] is of size %d and [%d] is of size %d" % (i-1, ncolumns, i, len(row)))
-    ret = Matrix(len(array), len(array[0]))
-    ret.array = array
-    return ret
+        if len(row) != columns:
+            raise ValueError("Dimensions are not consistent: row [%d] is of "
+            "size %d and row [%d] is of size %d"
+            % (i-1, columns, i, len(row)))
+            inputTypeAssert([[el, (int, float)] for el in row])
+    return [row, columns]
 
+class Matrix(object):
 
-class Matrix:
+    def __init__(self, fill, rows = None, columns = None):
+        """
+        Give an integer to fill a matrix of x rows by y columns
+        or give a rectangular array (consistent number of columns)
+        """
+        self.nrows = rows
+        self.ncolumns = columns
+        if type(fill) is list:
+            [self.nrows, self.ncolumns] = arrayShapeAssert(fill)
+            self.grid = fill
+        elif isinstance(fill, (float, int)):
+            if self.nrows == None or self.ncolumns == None or self.nrows <= 0 \
+                or self.ncolumns <= 0:
+                raise ValueError("If value provided is not an array"
+                " the matrix's size must be specified by `rows` and `columns` "
+                "greater than 0")
+            self.newMatrix(fill, self.nrows, self.ncolumns)
+        else:
+            raise ValueError("Bad Value type for the first parameter")
 
-    def __init__(self, x, y, default=0):
-        self.nrows = x
-        self.ncols  = y
-        self.array = [[default for point in range(self.ncols)] for row in range(self.nrows)]
-
-    
+    def newMatrix(self, value, rows, columns):
+        inputTypeAssert([[rows, int], [columns, int], [value, (float, int)]])
+        self.nrows = rows
+        self.ncols = columns
+        self.grid  = [[value for point in range(self.ncols)] for row in range(self.nrows)]
