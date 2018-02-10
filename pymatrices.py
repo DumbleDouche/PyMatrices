368551ccc4b3558fc86f3ce4f52034ea9d5e18e3
DumbleDouche <www.ronan.c@gmail.com>
2018-02-10 02:16:00 +0100

    Added the matrix function

--- pymatrices.py
+++ pymatrices.py
@@ -1,10 +1,27 @@
 """
-Very modest try at making a library on Matrix operations in Python
+Very modest attempt at making a library on Matrix operations in Python
 
 """
 
 __version__ = "1.0"
 __all__     = ['Matrix']
+
+def   matrix(array):
+    try:
+        nrows = len(array)
+    except TypeError:
+        raise TypeError("Object of type", type(array), "should a two-dimension array")
+    try:
+        ncolumns = len(array[0])
+    except IndexError:
+        raise IndexError("Object should be a two-dimensionnal array")
+    for i, row in enumerate(array):
+        if len(row) != ncolumns:
+            raise ValueError("Dimensions are not consistent: [%d] is of size %d and [%d] is of size %d" % (i-1, ncolumns, i, len(row)))
+    ret = Matrix(len(array), len(array[0]))
+    ret.array = array
+    return ret
+
 
 class Matrix:
 
@@ -12,3 +29,5 @@ class Matrix:
         self.nrows = x
         self.ncols  = y
         self.array = [[default for point in range(self.ncols)] for row in range(self.nrows)]
+
+    
